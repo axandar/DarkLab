c3f60a180e2915a2e8506ed7b97811e1c7e2b490
Kajetan Jóźwiak <unvios22@gmail.com>
2019-12-06 22:11:22 +0100

    Add Tags.cs

---
+++ Assets/Scripts/Readonly/Tags.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags : MonoBehaviour {
+	public static readonly string PLAYER = "Player";
+	public static readonly string MAIN_CAMERA = "MainCamera";
+	public static readonly string GAME_CONTROLLER = "GameController";
+}
