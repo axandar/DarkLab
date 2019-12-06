1679349007da6c8b4fc11bbf2b37a6d83edf2a78
Kajetan Jóźwiak <unvios22@gmail.com>
2019-12-06 22:40:04 +0100

    Turret looks at mouse

--- Assets/Scripts/Mechanics/TurretControl.cs
+++ Assets/Scripts/Mechanics/TurretControl.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
-public class TurretControl : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TurretControl : MonoBehaviour {
+    private Camera _mainCamera;
+
+    private void Start() {
+        _mainCamera = GameObject.FindGameObjectWithTag(Tags.MAIN_CAMERA).GetComponent<Camera>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        var toMouseVector = Input.mousePosition - _mainCamera.WorldToScreenPoint(transform.position);
+        var targetAngle = Mathf.Atan2(toMouseVector.y, toMouseVector.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(targetAngle, Vector3.forward); 
     }
 }
