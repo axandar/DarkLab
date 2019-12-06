8d64ed50515629ed822b9a4e230be34377d8fbb3
Cainie <karol.marszalik@gmail.com>
2019-12-06 23:55:32 +0100

    WIP: enemiesGoToTurret

--- Assets/Scripts/Readonly/Tags.cs
+++ Assets/Scripts/Readonly/Tags.cs
@@ -6,4 +6,5 @@ public class Tags : MonoBehaviour {
 	public static readonly string PLAYER = "Player";
 	public static readonly string MAIN_CAMERA = "MainCamera";
 	public static readonly string GAME_CONTROLLER = "GameController";
+	public static readonly string TURRET = "Turret";
 }
