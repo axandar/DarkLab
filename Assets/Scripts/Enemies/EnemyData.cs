98b2e1ba1890a9de4ce43f4c7df465da0c19c00d
Kajetan Jóźwiak <unvios22@gmail.com>
2020-12-03 04:23:13 +0100

    Cleanup project files

---
+++ Assets/Scripts/Enemies/EnemyData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Enemies {
+	[CreateAssetMenu(menuName = "EnemyData")]
+	public class EnemyData : ScriptableObject {
+		public int maxHealth;
+		public float speed;
+		public float minTimeForNextShake;
+		public float maxTimeForNextShake;
+		public float targetSearchOffsetX;
+		public float targetSearchOffsetY;
+		public int pointsForEnemy;
+		public int dealtDamage;
+		public GameObject particleSystemPrefab;
+		public GameObject tinyParticleSystemPrefab;
+	}
+}
