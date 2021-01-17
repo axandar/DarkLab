601921d69a1dd9aa4d31c4fd38e730036bb2ae32
Kajetan Jóźwiak <unvios22@gmail.com>
2021-01-17 03:41:55 +0100

    Add ScoreManager && score UI display

---
+++ Assets/Scripts/Mechanics/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+	[SerializeField] private TextMeshProUGUI scoreTextDisplay;
+	private int _score;
+
+	public void RegisterEnemy(Enemy enemy) {
+		//todo subscribe to onkilled
+	}
+	
+	private void Start() {
+		ResetScore();
+	}
+
+	private void OnEnemyKilled(int enemyScoreWorth) {
+		_score += enemyScoreWorth;
+		UpdateScoreDisplay();
+	}
+
+	private void UpdateScoreDisplay() {
+		scoreTextDisplay.text = _score.ToString();
+	}
+	
+	private void ResetScore() {
+		_score = 0;
+	}
+}
